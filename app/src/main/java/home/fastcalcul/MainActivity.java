9b1f3593e001221e27a76ed04abdadca0da8c60e
olivier <rozan.olivier@gmail.com>
2017-07-16 21:26:04 +0200

    Cleaned code and layout

--- app/src/main/java/home/fastcalcul/MainActivity.java
+++ app/src/main/java/home/fastcalcul/MainActivity.java
@@ -43,15 +43,13 @@ public class MainActivity extends AppCompatActivity {
     private CountDownTimer countDownTimer;
     private Animation goodAnim, badAnim;
     public String mode;
-    public String beginDialogTitle/*, score_name*/;
+    public String beginDialogTitle;
 
     private EditText TName;
 
     private HighScores highScore;
 
     private static final String PREFS = "PREFS";
-    private static final String PREFS_SCORE = "PREFS_SCORE";
-    private static final String PREFS_NAME = "PREFS_NAME";
     SharedPreferences sharedPreferences;
 
     @Override
@@ -60,28 +58,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sharedPreferences = getBaseContext().getSharedPreferences(PREFS, MODE_PRIVATE);
-//
-//        //objectif : sauvegarder 1 seule fois le nom et l'age de l'utilisateur
-//
-//        //pour cela, on commence par regarder si on a déjà des éléments sauvegardés
-//        if (sharedPreferences.contains(PREFS_SCORE) && sharedPreferences.contains(PREFS_NAME)) {
-//
-//            int score = sharedPreferences.getInt(PREFS_SCORE, 0);
-//            String name = sharedPreferences.getString(PREFS_NAME, null);
-//            Toast.makeText(this, name + "    " + score, Toast.LENGTH_SHORT).show();
-//
-//        } else {
-//            //si aucun utilisateur n'est sauvegardé, on ajouter [24,florent]
-//            sharedPreferences
-//                    .edit()
-//                    .putInt(PREFS_SCORE, 24)
-//                    .putString(PREFS_NAME, "florent")
-//                    .apply();
-//
-//            Toast.makeText(this, "Sauvegardé, relancez l'application pour voir le résultat", Toast.LENGTH_SHORT).show();
-//        }
-
-        //sharedPreferences.edit().clear().apply();
 
         mode = (String) getIntent().getExtras().get("mode");
 
@@ -112,11 +88,11 @@ public class MainActivity extends AppCompatActivity {
         numberOfBadAnswers = 0;
         play = true;
         level = 1;
-        timer = 5000;
+        timer = 30000;
         timeWhenPaused = 0;
-        //totalDialog = 0;
+
         beginDialogTitle = "";
-        //score_name = "";
+
         initCountDownTimer();
 
         highScore = new HighScores();
@@ -267,24 +243,12 @@ public class MainActivity extends AppCompatActivity {
                     TGoodAnswers.setVisibility(View.VISIBLE);
                     TGoodAnswers.startAnimation(goodAnim);
 
-                    // Adds 10 seconds to timer every 10 good answers
-//                    if (numberOfGoodAnswers % 10 == 0) {
-//                        countDownTimer.cancel();
-//                        timer += 30000;
-//                        initCountDownTimer();
-//                    }
-
                     TGoodAnswers.setText("Correct " + String.valueOf(numberOfGoodAnswers));
                 } else {
                     numberOfBadAnswers++;
 
                     TBadAnswers.setVisibility(View.VISIBLE);
                     TBadAnswers.startAnimation(badAnim);
-
-//                    countDownTimer.cancel();
-//                    // Removes 1 second to timer for each wrong answer
-//                    timer -= 1000;
-//                    initCountDownTimer();
 
                     TBadAnswers.setText("Errors " + String.valueOf(numberOfBadAnswers));
 
